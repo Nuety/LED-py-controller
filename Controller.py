2432a3efed2f2fb829554b62f7e26d8412d676d0
Asger Fihl Hedegaard Nielsen <asger@fihl.net>
2024-10-20 11:37:18 +0200

    Basic server created

--- Controller.py
+++ Controller.py
@@ -11,13 +11,13 @@ def start_cpu():
 def default_case():
     print("Unknown instruction")
 
+switcher = {
+    'maze': start_maze,
+    'cpu': start_cpu,
+}
 
 def ServerLoop():
 
-    switcher = {
-        'Maze': start_maze,
-        'cpu': start_cpu,
-    }
 
     # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,7 +25,7 @@ def ServerLoop():
 
     host = '0.0.0.0'
     port = 5550
-    
+
     server_socket.bind((host, port))
 
     # Listen for a connection only 1 connection
@@ -41,11 +41,12 @@ def ServerLoop():
         try:
             while True:
                 # Wait for instructions from the client
-                message = client_socket.recv(1024)
-                
+                message = client_socket.recv(1024).decode()
+
                 if not message:
                     # If no message is received, break the loop (client disconnected)
                     print("Client disconnected")
+                    print("Waiting for client connection...")
                     break
                 
                 # Print received message
@@ -54,10 +55,10 @@ def ServerLoop():
                 # Send an acknowledgment back to the client
                 action = switcher.get(message, default_case)
                 if action != default_case:    
-                    client_socket.send(f"Starting program: {message}")
+                    client_socket.send(f"Starting program: {message}".encode())
                     action()
                 else:
-                    client_socket.send(f"Unknown command received")
+                    client_socket.send("Unknown command received".encode())
         
         except socket.error as e:
             print("Socket error:", e)
@@ -66,8 +67,6 @@ def ServerLoop():
             # Close the client socket when the loop ends
             client_socket.close()
 
-    # Close the server socket (optional in this example since it's infinite)
-    # server_socket.close()
 
 
 
